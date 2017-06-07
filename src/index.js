d6516f450d26d3315e0312313c5e9beb94d52e5c
Alex J <alexjovermorales@gmail.com>
2017-06-07 16:29:38 +0200

    feat: Add tslint-eslint-rules

--- src/index.js
+++ src/index.js
@@ -26,6 +26,15 @@ module.exports = {
     "jsx-wrap-multiline": false,
 
     // tslint-eslint-rules (https://github.com/buzinas/tslint-eslint-rules)
-
+    "block-spacing": false,
+    "brace-style": false,
+    "no-multi-spaces": false,
+    "object-curly-spacing": false,
+    "space-in-parens": false,
+    "ter-arrow-parens": false,
+    "ter-arrow-spacing": false,
+    "ter-indent": false,
+    "ter-max-len": false,
+    "ter-no-irregular-whitespace": false
   }
 }
