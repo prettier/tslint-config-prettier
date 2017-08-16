cb045fc7eb39360c3bff93d191eed084bde4edd5
ikatyang <ikatyang@gmail.com>
2017-08-16 16:43:44 +0800

    refactor: splitting rulesets

---
+++ src/tslint-eslint-rules/rules.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "block-spacing": false,
+  "brace-style": false,
+  "no-multi-spaces": false,
+  "object-curly-spacing": false,
+  "space-in-parens": false,
+  "ter-arrow-parens": false,
+  "ter-arrow-spacing": false,
+  "ter-indent": false,
+  "ter-max-len": false,
+  "ter-no-irregular-whitespace": false,
+};
