b4e99afb5616904b924d2fd8c1692685e8f58aac
Ika <ikatyang@gmail.com>
2018-03-09 12:43:26 +0800

    feat(checker): better conflict-rules message

--- bin/check.js
+++ bin/check.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
-require('../lib/checker').check(process.argv[2]);
+
+try {
+  require("../lib/checker").check(process.argv[2]);
+} catch (e) {
+  if (e.name === "ConflictRules") {
+    console.log(e.message);
+    process.exitCode = 1;
+  } else {
+    throw e;
+  }
+}
